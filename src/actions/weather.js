3944d61c4b921e2872867dd1e515d7b4f7d899c5
ksamy90 <ksamy90@aol.com>
2021-12-11 05:52:55 +0300

    add api key

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = "76eaf7e6445f9efeaa807693e2d7a46b";
 const API_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const fetchWeather = (city) => {
